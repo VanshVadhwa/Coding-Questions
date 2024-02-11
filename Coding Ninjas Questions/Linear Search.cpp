43b3814ef41b5e056d73ad04b58c1ff06586fde9
Vansh Vadhwa <vvad0647@gmail.com>
2024-02-11 15:02:01 +0530

    Add files via upload

---
+++ Coding Ninjas Questions/Linear Search.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int *arr, int n, int x)
+{
+    for(int i=0;i<n;i++) {
+        if(x==arr[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
