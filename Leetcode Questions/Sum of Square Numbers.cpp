2a49f38bc360fc276e780a07f77631faa2ae29d2
Vansh Vadhwa <vvad0647@gmail.com>
2024-06-25 20:20:26 +0530

    Update Sum of Square Numbers.cpp

--- Leetcode Questions/Sum of Square Numbers.cpp
+++ Leetcode Questions/Sum of Square Numbers.cpp
@@ -1,12 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
 class Solution {
 public:
     bool judgeSquareSum(int c) {
         long long start = 0;
         long long end = sqrt(c);
 
-        while(start<=end) {
-            long long n = start*start;
-            long long m = end*end;
+        while(start <= end) {
+            long long n = start * start;
+            long long m = end * end;
             if(m + n == c) {
                 return true;
             }
@@ -20,3 +25,21 @@ public:
         return false;
     }
 };
+
+int main() {
+    Solution solution;
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    bool result = solution.judgeSquareSum(number);
+
+    if(result) {
+        cout << number << " can be expressed as the sum of two squares." << endl;
+    } else {
+        cout << number << " cannot be expressed as the sum of two squares." << endl;
+    }
+
+    return 0;
+}
