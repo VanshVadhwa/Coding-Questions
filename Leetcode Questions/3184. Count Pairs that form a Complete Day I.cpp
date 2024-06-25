cad1e5a5e2c11127d6d8d65b9c7262429ce63c93
Vansh Vadhwa <vvad0647@gmail.com>
2024-06-25 20:19:13 +0530

    Update 3184. Count Pairs that form a Complete Day I.cpp

--- Leetcode Questions/3184. Count Pairs that form a Complete Day I.cpp
+++ Leetcode Questions/3184. Count Pairs that form a Complete Day I.cpp
@@ -1,14 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int countCompleteDayPairs(vector<int>& hours) {
-        unordered_map<int, int> map;
-
         int n = hours.size();
         int ans = 0;
 
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                if((hours[i]+hours[j])%24==0) {
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if((hours[i] + hours[j]) % 24 == 0) {
                     ans++;
                 }
             }
@@ -16,3 +19,14 @@ public:
         return ans;
     }
 };
+
+int main() {
+    Solution solution;
+    vector<int> hours = {1, 23, 12, 12, 24, 0};
+
+    int result = solution.countCompleteDayPairs(hours);
+
+    cout << "Number of complete day pairs: " << result << endl;
+
+    return 0;
+}
