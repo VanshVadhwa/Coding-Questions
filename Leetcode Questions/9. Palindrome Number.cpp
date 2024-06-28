564e76b6fb076a46a350092ebfcb03abb0572938
Vansh Vadhwa <vvad0647@gmail.com>
2024-06-28 08:59:35 +0530

    Update 9. Palindrome Number.cpp

--- Leetcode Questions/9. Palindrome Number.cpp
+++ Leetcode Questions/9. Palindrome Number.cpp
@@ -1,34 +1,61 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-    int reverse(int num) {
-        if (num < 0) {
-            return false;
-        }
-        long long rev = 0;
+// class Solution {
+//     int reverse(int num) {
+//         if (num < 0) {
+//             return false;
+//         }
+//         long long rev = 0;
 
-        while (num != 0) {
-            int digit = num % 10;
-            rev = rev * 10 + digit;
-            num = num / 10;
-        }
-        return rev;
-    }
+//         while (num != 0) {
+//             int digit = num % 10;
+//             rev = rev * 10 + digit;
+//             num = num / 10;
+//         }
+//         return rev;
+//     }
+// public:
+//     bool isPalindrome(int x) {
+//         int num = reverse(x);
+//         return num == x;
+//     }
+// };
+class Solution {
 public:
     bool isPalindrome(int x) {
-        int num = reverse(x);
-        return num == x;
+        if(x<0) {
+            return false;
+        }
+
+        int ans = 0;
+        int rem;
+        int original = x;
+
+        while(x!=0) {
+            rem = x%10;
+            x /= 10;
+            if(ans > INT_MAX/10) {
+                return 0;
+            }
+            ans = ans*10 + rem;
+        }
+
+        if(ans == original) {
+            return true;
+        }
+
+        return false;
     }
 };
 
 int main() {
     Solution solution;
-    int x = 121; // Example input
+    int x = 121; 
     if (solution.isPalindrome(x)) {
-        std::cout << x << " is a palindrome." << std::endl;
+        cout << x << " is a palindrome." << endl;
     } else {
-        std::cout << x << " is not a palindrome." << std::endl;
+        cout << x << " is not a palindrome." << endl;
     }
     return 0;
 }
