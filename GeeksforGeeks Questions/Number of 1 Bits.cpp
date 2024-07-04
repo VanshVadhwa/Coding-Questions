83fdd6d6d629129a8f19e9c5856ecb67ecddc601
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-04 22:20:23 +0530

    Create Number of 1 Bits.cpp

---
+++ GeeksforGeeks Questions/Number of 1 Bits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int count = 0;
+        while(N) {
+            int rem = 0;
+            if(N%2!=0) {
+                count++;
+            }
+            N /= 2;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
