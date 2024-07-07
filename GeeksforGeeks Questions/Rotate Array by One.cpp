f2f17e5121b199ab5462c78731585ab59132321e
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-07 16:37:35 +0530

    Create Rotate Array by One.cpp

---
+++ GeeksforGeeks Questions/Rotate Array by One.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        
+        int temp = arr[n-1];
+        
+        for(int i=n-1;i>0;i--) {
+            arr[i]=arr[i-1];
+        }
+        arr[0] = temp;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.rotate(arr);
+        for (long long i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
