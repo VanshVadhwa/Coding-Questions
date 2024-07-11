733a95fe77c36051d3e4b9a58af7236d2977370d
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-12 00:08:19 +0530

    Create Insertion Sort.cpp

---
+++ GeeksforGeeks /Algorithms/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void insert(int arr[], int n)
+    {
+        //code here
+        for(int i=1;i<n;i++) {
+            for(int j=i;j>0;j--) {
+                if(arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        insert(arr, n);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
