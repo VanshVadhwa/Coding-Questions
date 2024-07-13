b635c91de5d53a566382c6908435924ea625a51e
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-14 01:00:16 +0530

    Updated

--- Leetcode /LC 000VV Series/33. Search in Rotated Sorted Array.cpp
+++ Leetcode /LC 000VV Series/33. Search in Rotated Sorted Array.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
+    int search(vector<int>& nums, int target) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        
         int n = nums.size();
 
-        int start = 0, end = n-1, ans = nums[0];
+        int start = 0, end = n-1;
 
-        while(start <= end)
+        while(start <= end) 
         {
-            int mid = start+(end-start)/2;
+            int mid = start + (end-start)/2;
 
-            if(nums[mid]>=nums[0]) {
-                start = mid+1;
+            if(nums[mid]==target) {
+                return mid;
             }
-            else {
-                ans = nums[mid];
-                end = mid-1;
+
+            else if(nums[mid]>=nums[0]) 
+            {
+                if(target <= nums[mid] && target >= nums[start]) 
+                {
+                    end = mid-1;
+                }
+                else 
+                {
+                    start = mid+1;
+                }
+            }
+
+            else 
+            {
+                if(target<=nums[end] && target>=nums[mid])
+                {
+                    start = mid+1;
+                }
+                else 
+                {
+                    end = mid-1;
+                }
             }
         }
-        return ans;
+
+        return -1;
     }
 };
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int n;
-    cin >> n;
-    
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    Solution sol;
-    int ans = sol.findMin(arr);
-    cout << ans << endl;
-
-    return 0;
-}
