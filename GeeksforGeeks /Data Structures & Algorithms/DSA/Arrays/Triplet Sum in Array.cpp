f0a71b959aa8b08ce24f354c24f2f9763b8f4f90
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-20 23:34:10 +0530

    Update Triplet Sum in Array.cpp

--- GeeksforGeeks /Data Structures & Algorithms/DSA/Arrays/Triplet Sum in Array.cpp
+++ GeeksforGeeks /Data Structures & Algorithms/DSA/Arrays/Triplet Sum in Array.cpp
@@ -12,33 +12,57 @@ class Solution {
     bool find3Numbers(int arr[], int n, int x) {
         // Your Code Here
         sort(arr, arr+n);
+        
         int ans = 0;
         
-        for(int i=0;i<n-2;i++)
-        {
-            for(int j=i+1;j<n-1;j++)
+        for(int i=0;i<n-2;i++) 
+        { 
+            ans = x - arr[i];
+            
+            int start = i+1, end = n-1;
+            
+            while(start < end)
             {
-                ans = x - (arr[i]+arr[j]);
-                
-                int start = j+1, end = n-1;
-                
-                while(start <= end)
+                if(arr[start]+arr[end]==ans) 
                 {
-                    int mid = start + (end-start)/2;
-                    
-                    if(arr[mid]==ans) {
-                        return 1;
-                    }
-                    if(arr[mid]<ans) {
-                        start = mid+1;
-                    }
-                    else {
-                        end = mid-1;
-                    }
+                    return 1;
+                }
+                else if(arr[start] + arr[end] < ans) 
+                {
+                    start++;
+                }
+                else {
+                    end--;
                 }
             }
         }
-         return 0;
+        return 0;
+        
+        // for(int i=0;i<n-2;i++)
+        // {
+        //     for(int j=i+1;j<n-1;j++)
+        //     {
+        //         ans = x - (arr[i]+arr[j]);
+                
+        //         int start = j+1, end = n-1;
+                
+        //         while(start <= end)
+        //         {
+        //             int mid = start + (end-start)/2;
+                    
+        //             if(arr[mid]==ans) {
+        //                 return 1;
+        //             }
+        //             if(arr[mid]<ans) {
+        //                 start = mid+1;
+        //             }
+        //             else {
+        //                 end = mid-1;
+        //             }
+        //         }
+        //     }
+        // }
+        //  return 0;
     }
 };
 
