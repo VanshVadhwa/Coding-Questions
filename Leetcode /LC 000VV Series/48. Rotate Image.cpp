cb8dafc879748574a34bed5a8a8c71d1b0eaba25
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-23 23:37:23 +0530

    Update 48. Rotate Image.cpp

--- Leetcode /LC 000VV Series/48. Rotate Image.cpp
+++ Leetcode /LC 000VV Series/48. Rotate Image.cpp
@@ -1,20 +1,70 @@
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+
+//         //Brute Force 
+//         class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+
+        // vector<vector<int>> ans(n, vector<int>(n,1));
+
+        // for(int j=n-1;j>=0;j--)
+        // {
+        //     int rowIndex = n-1-j;
+        //     int colIndex = 0;
+        //     for(int i=0;i<n;i++)
+        //     {
+        //         ans[rowIndex][i] = matrix[i][j];
+        //     }
+        // }
+
+        // reverse(ans.begin(), ans.end());
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     reverse(ans[i].begin(), ans[i].end());
+        // }
+
+        // matrix = ans;
+
+        
+        //More Optimized
+        // vector<vector<int>> ans(n, vector<int>(n,1));
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         ans[j][n-1-i] = matrix[i][j];
+        //     }
+        // }
+
+        // matrix = ans;
+
+        // Most Optimized (O(1)) Space
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
-                //Swapping the elements 
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        //Transpose
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n/2;j++) {
-                swap(matrix[i][j], matrix[i][n-j-1]);
+        //Reverse Rows
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                swap(matrix[i][j], matrix[i][n-1-j]);
             }
         }
     }
