8da42fb33ebabbb8f020d6695cde9076b12d46d0
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-28 11:58:46 +0530

    Create 50A - Domino Piling.cpp

---
+++ Codeforces/50A - Domino Piling.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b;
+    cin >> a >> b;
+    cout << a*b/2 << endl;
+    return 0;
+}
