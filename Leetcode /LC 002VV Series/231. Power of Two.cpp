7e9f3d957115e6ee27798c4345c84ff2fd935258
Vansh Vadhwa <vvad0647@gmail.com>
2024-08-06 13:35:35 +0530

    Update 231. Power of Two.cpp

--- Leetcode /LC 002VV Series/231. Power of Two.cpp
+++ Leetcode /LC 002VV Series/231. Power of Two.cpp
@@ -1,24 +1,32 @@
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        if(n<=0) return false;
+        return 1073741824 % n == 0; 
     }
 };
 
-int main() {
-    Solution solution;
-    int num;
-    cout << "Enter a number to check if it is a power of two: ";
-    cin >> num;
+// #include <iostream>
+// using namespace std;
 
-    if (solution.isPowerOfTwo(num)) {
-        std::cout << num << " is a power of two." << endl;
-    } else {
-        std::cout << num << " is not a power of two." << endl;
-    }
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         return n > 0 && (n & (n - 1)) == 0;
+//     }
+// };
+
+// int main() {
+//     Solution solution;
+//     int num;
+//     cout << "Enter a number to check if it is a power of two: ";
+//     cin >> num;
+
+//     if (solution.isPowerOfTwo(num)) {
+//         std::cout << num << " is a power of two." << endl;
+//     } else {
+//         std::cout << num << " is not a power of two." << endl;
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
