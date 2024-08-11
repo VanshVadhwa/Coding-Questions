4f940701300cddaf351fb04c73f7f1b4c351b1ae
Vansh Vadhwa <vvad0647@gmail.com>
2024-08-11 18:49:35 +0530

    Update 03. Longest Substring Without Repeating Characters.cpp

--- Leetcode /LC 000VV Series/03. Longest Substring Without Repeating Characters.cpp
+++ Leetcode /LC 000VV Series/03. Longest Substring Without Repeating Characters.cpp
@@ -1,32 +1,56 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.length();
+//         if(n==0) return 0;
+//         vector<int> alpha(256,-1);
+//         int ans = 1;
+
+//         for(int i=1;i<n;i++)
+//         {
+//             int count = 1; 
+//             alpha[s[i-1]]++;
+//             for(int j=i;j<n;j++)
+//             {
+//                 alpha[s[j]]++;
+//                 if(s[j]!=s[j-1] && alpha[s[j]]==0) 
+//                 {
+//                     count++;
+//                 }
+//                 else {
+//                     break;
+//                 }
+//             }
+//             ans = max(ans,count);
+//             for(int i=0;i<n;i++)
+//             {
+//                 alpha[s[i]] = -1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.length();
-        if(n==0) return 0;
-        vector<int> alpha(256,-1);
-        int ans = 1;
+        vector<bool> alpha(256, 0);
+        int first = 0, second = 0, len = 0;
 
-        for(int i=1;i<n;i++)
+        while(second < n)
         {
-            int count = 1; 
-            alpha[s[i-1]]++;
-            for(int j=i;j<n;j++)
+            while(alpha[s[second]])
             {
-                alpha[s[j]]++;
-                if(s[j]!=s[j-1] && alpha[s[j]]==0) 
-                {
-                    count++;
-                }
-                else {
-                    break;
-                }
+                alpha[s[first]] = 0;
+                first++;
             }
-            ans = max(ans,count);
-            for(int i=0;i<n;i++)
-            {
-                alpha[s[i]] = -1;
-            }
+            alpha[s[second]]=1;
+            len = max(len, second-first+1);
+            second++;
         }
-        return ans;
+
+        return len;
     }
 };
