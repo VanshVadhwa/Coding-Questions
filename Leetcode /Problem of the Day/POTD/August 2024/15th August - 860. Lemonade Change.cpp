3989db657009477c0586ffa3a77705295357ef97
Vansh Vadhwa <vvad0647@gmail.com>
2024-08-15 12:34:16 +0530

    Create 15th August - 860. Lemonade Change.cpp

---
+++ Leetcode /Problem of the Day/POTD/August 2024/15th August - 860. Lemonade Change.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n = bills.size();
+        int count_5=0,count_10=0,count_20=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(bills[i]==5) {
+                count_5 += 1;
+            }
+            if(bills[i]==10) {
+                count_10 += 1;
+                if(count_5 < 1) return false;
+                else count_5 -= 1;
+            }
+            if(bills[i]==20)
+            {
+                count_20 += 1;
+                if(count_5 >=1 && count_10 >= 1) {
+                    count_5 -= 1;
+                    count_10 -= 1;
+                }
+                else if(count_5 >= 3) {
+                    count_5 -= 3;
+                }
+                else return false;
+            }
+
+        }
+        return true;
+    }
+};
