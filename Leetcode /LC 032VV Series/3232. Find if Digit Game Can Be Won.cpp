6996a5e148cbfe5143597645d8d2179519879739
Vansh Vadhwa <vvad0647@gmail.com>
2024-08-18 22:36:49 +0530

    Update 3232. Find if Digit Game Can Be Won.cpp

--- Leetcode /LC 032VV Series/3232. Find if Digit Game Can Be Won.cpp
+++ Leetcode /LC 032VV Series/3232. Find if Digit Game Can Be Won.cpp
@@ -3,6 +3,7 @@ public:
     bool canAliceWin(vector<int>& nums) {
         int n = nums.size();
         int single = 0, doub = 0;
+
         for(int i=0;i<n;i++)
         {
             if(nums[i]<10)
@@ -11,7 +12,6 @@ public:
             doub += nums[i];
         }
 
-        if(single > doub || single<doub) return true;
-        return false;
+        return (single > doub || single < doub);
     }
 };
