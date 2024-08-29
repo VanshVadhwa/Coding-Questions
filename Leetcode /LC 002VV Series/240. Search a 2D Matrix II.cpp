29ab0667a3b860965ce88dd59da733ce6912e1f3
Vansh Vadhwa <vvad0647@gmail.com>
2024-08-29 20:34:04 +0530

    Update 240. Search a 2D Matrix II.cpp

--- Leetcode /LC 002VV Series/240. Search a 2D Matrix II.cpp
+++ Leetcode /LC 002VV Series/240. Search a 2D Matrix II.cpp
@@ -1,18 +1,46 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int row = matrix.size();
+//         int col = matrix[0].size();
+
+//         int i=0, j=col-1;
+
+//         while(i < row && j>= 0)
+//         {
+//             if(matrix[i][j]==target) return true;
+//             else if(matrix[i][j]<target) i++;
+//             else j--;
+//         }
+
+//         return false;   
+//     }
+// };
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
 
-        int i=0, j=col-1;
-
-        while(i < row && j>= 0)
+        for(int i=0;i<col;i++)
         {
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]<target) i++;
-            else j--;
-        }
+            int start = 0, end = row-1;
+            while(start <= end)
+            {
+                int mid = start + (end-start)/2;
 
-        return false;   
+                if(matrix[mid][i]==target) {
+                    return true;
+                }
+                else if(matrix[mid][i]<target) {
+                    start = mid+1;
+                }
+                else {
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
     }
 };
