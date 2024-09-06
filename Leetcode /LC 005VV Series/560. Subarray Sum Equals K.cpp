a2f68e9eee9540667e68eb4c0e0dbbe38627b268
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-06 19:36:31 +0530

    Create 560. Subarray Sum Equals K.cpp

---
+++ Leetcode /LC 005VV Series/560. Subarray Sum Equals K.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+
+        for(int i=0;i<n;i++) 
+        {
+            int sum = 0;
+            for(int j=i;j<n;j++) {
+                sum += nums[j];
+                if(sum==k) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
