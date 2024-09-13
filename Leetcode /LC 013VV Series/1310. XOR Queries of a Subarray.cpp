d10550231550b2178f2164d0862c548df333032d
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-13 14:40:05 +0530

    Create 1310. XOR Queries of a Subarray.cpp

---
+++ Leetcode /LC 013VV Series/1310. XOR Queries of a Subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        vector<int>prefix(n);
+        prefix[0] = arr[0];
+        for(int i=1;i<n;i++) {
+            prefix[i] = prefix[i-1]^arr[i];
+        }
+
+        vector<int> ans(queries.size(),0);
+        for(int i=0;i<queries.size();i++)
+        {
+            int left = queries[i][0];
+            int right = queries[i][1];
+            if (left == 0) {
+                ans[i] = prefix[right];
+            } else {
+                ans[i] = prefix[right] ^ prefix[left - 1];
+            }
+        }
+
+        return ans;
+    }
+};
