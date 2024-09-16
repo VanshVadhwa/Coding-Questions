8b2cb29166bb791283277a27cb6f28102a278f31
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-16 23:09:25 +0530

    Update 1832. Check if the Sentence is Pangram.cpp

--- Leetcode /LC 018VV Series/1832. Check if the Sentence is Pangram.cpp
+++ Leetcode /LC 018VV Series/1832. Check if the Sentence is Pangram.cpp
@@ -9,21 +9,42 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     bool checkIfPangram(string sentence) {
+//         vector<bool> alpha(26,0);
+
+//         int n = sentence.size();
+
+//         for(int i=0;i<n;i++)
+//         {
+//             alpha[sentence[i]-'a'] = 1;
+//         }
+//         for(int i=0;i<26;i++)
+//         {
+//             if(alpha[i]==0) return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        vector<bool> alpha(26,0);
-
         int n = sentence.size();
+        if(n<26) return false;
 
-        for(int i=0;i<n;i++)
-        {
-            alpha[sentence[i]-'a'] = 1;
+        vector<bool> alpha(26,false);
+        int count = 0;
+
+        for(char c : sentence) {
+            if(!alpha[c-'a']) {
+                count++;
+                alpha[c-'a'] = true;
+            }
+            if(count==26) return true;
         }
-        for(int i=0;i<26;i++)
-        {
-            if(alpha[i]==0) return false;
-        }
-        return true;
+
+        return false;
     }
 };
