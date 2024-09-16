48915ba9510ce26b905ff8fe35cb05f389c12fd4
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-17 01:28:26 +0530

    Update 415. Add Strings.cpp

--- Leetcode /LC 004VV Series/415. Add Strings.cpp
+++ Leetcode /LC 004VV Series/415. Add Strings.cpp
@@ -1,45 +1,69 @@
-class Solution {
-private:
-    string add(string num1, string num2) {
-        int index1 = num1.size()-1;
-        int index2 = num2.size()-1;
+// class Solution {
+// private:
+//     string add(string num1, string num2) {
+//         int index1 = num1.size()-1;
+//         int index2 = num2.size()-1;
 
-        int carry = 0, sum = 0;
+//         int carry = 0, sum = 0;
         
-        string ans = "";
+//         string ans = "";
 
-        while(index2 >= 0)
-        {
-            sum = (num1[index1]-'0') + (num2[index2]-'0') + carry;
-            carry = sum/10;
-            char c = sum%10 + '0';
-            ans += c;
-            index2--;
-            index1--;
-        }
+//         while(index2 >= 0)
+//         {
+//             sum = (num1[index1]-'0') + (num2[index2]-'0') + carry;
+//             carry = sum/10;
+//             char c = sum%10 + '0';
+//             ans += c;
+//             index2--;
+//             index1--;
+//         }
 
-        while(index1 >= 0)
-        {
-            sum = (num1[index1]-'0') + carry;
-            carry = sum/10;
-            char c = sum%10 + '0';
-            ans += c;
-            index1--;
-        }
+//         while(index1 >= 0)
+//         {
+//             sum = (num1[index1]-'0') + carry;
+//             carry = sum/10;
+//             char c = sum%10 + '0';
+//             ans += c;
+//             index1--;
+//         }
 
-        if(carry) ans += '1';
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
+//         if(carry) ans += '1';
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
 
+// public:
+//     string addStrings(string num1, string num2) {
+//         if(num1.size()<num2.size()) {
+//             return add(num2, num1);
+//         }
+//         else {
+//             return add(num1, num2);
+//         }
+//         return "";
+//     }
+// };
+
+
+class Solution {
 public:
     string addStrings(string num1, string num2) {
-        if(num1.size()<num2.size()) {
-            return add(num2, num1);
+        string ans = "";
+        int carry = 0;
+        int index1 = num1.length()-1;
+        int index2 = num2.length()-1;
+
+        while(index1>=0 || index2>=0 || carry)
+        {
+            int number1 = (index1>=0) ? num1[index1--]-'0' : 0;
+            int number2 = (index2>=0) ? num2[index2--]-'0' : 0;
+
+            int sum = number1 + number2 + carry;
+            ans += to_string(sum%10);
+            carry = sum/10;
         }
-        else {
-            return add(num1, num2);
-        }
-        return "";
+
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
