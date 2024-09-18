d2615da27e8df49bf2a6c1e335b92e09aa24f37b
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-18 16:33:12 +0530

    Create 15. 3Sum.cpp

---
+++ Leetcode /LC 000VV Series/15. 3Sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            int j = i+1, k=n-1;
+            while(j<k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                //two pointers
+                if(sum < 0) j++;
+                else if(sum > 0) k--;
+                else 
+                {
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
