0ec67923d08dff6b5085f0803c96aaa59ffc1fa7
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-21 20:32:17 +0530

    Create 1071. Greatest Common Divisor of Strings.java

---
+++ Leetcode /LC 010VV Series/1071. Greatest Common Divisor of Strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) {
+            return "";
+        }
+
+        int gcdlength = gcd(str1.length(),str2.length());
+
+        return str1.substring(0,gcdlength);
+    }
+    private int gcd(int a, int b)
+    {
+        if(b==0) {
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
