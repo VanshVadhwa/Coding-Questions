b22d7e4fc8bfe86d59d6b06281ae58829f38bfb9
Vansh Vadhwa <vvad0647@gmail.com>
2024-09-25 22:56:46 +0530

    Create 1832. Check if the Sentence is Pangram.java

---
+++ Leetcode /LC 018VV Series/1832. Check if the Sentence is Pangram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int arr[] = new int[26];
+        for(int i=0;i<sentence.length();i++) {
+            arr[sentence.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++) {
+            if(arr[i]==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
