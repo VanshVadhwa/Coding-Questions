465b80436114a6fa5d2c8701e872770ed85abc0a
Vansh Vadhwa <vvad0647@gmail.com>
2024-10-01 00:12:38 +0530

    Update 78. Subsets.cpp

--- Leetcode /LC 000VV Series/78. Subsets.cpp
+++ Leetcode /LC 000VV Series/78. Subsets.cpp
@@ -1,47 +1,15 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-class Solution {
-    void solve(vector<int>& nums, int index, int size, vector<vector<int>>& ans, vector<int>& temp) {
-        if(index >= size) {
-            ans.push_back(temp);
-            return;
-        }
-        // excluding
-        solve(nums, index + 1, size, ans, temp);
-
-        // including
-        temp.push_back(nums[index]);
-        solve(nums, index + 1, size, ans, temp);
-        temp.pop_back();
-    }
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        int size = nums.size();
-        int index = 0;
-        solve(nums, index, size, ans, temp);
-        return ans;
-    }
-};
-
-int main() {
-    Solution solution;
-    vector<int> nums = {1, 2, 3};
-
-    vector<vector<int>> result = solution.subsets(nums);
-
-    cout << "Subsets:" << endl;
-    for (int subset : result) {
-        cout << "{ ";
-        for (int num : subset) {
-            cout << num << " ";
-        }
-        cout << "}" << endl;
+void subsets(vector<int>&arr,int index, int n, vector<vector<int>>& ans, vector<int> temp)
+{
+    //base case
+    if(index==n) {
+        ans.push_back(temp);
+        return;
     }
 
-    return 0;
+    //not included
+    subsets(arr,index+1,n,ans,temp);
+
+    //included
+    temp.push_back(arr[index]);
+    subsets(arr,index+1,n,ans,temp);
 }
