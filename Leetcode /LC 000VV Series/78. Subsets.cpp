54341c484e6ff032b4890163296b4e569a40d067
Vansh Vadhwa <vvad0647@gmail.com>
2024-10-01 00:12:51 +0530

    Update 78. Subsets.cpp

--- Leetcode /LC 000VV Series/78. Subsets.cpp
+++ Leetcode /LC 000VV Series/78. Subsets.cpp
@@ -1,15 +1,25 @@
-void subsets(vector<int>&arr,int index, int n, vector<vector<int>>& ans, vector<int> temp)
-{
-    //base case
-    if(index==n) {
-        ans.push_back(temp);
-        return;
+class Solution {
+private:
+    void subsets(vector<int>& arr,int index, int n, vector<vector<int>>& ans, vector<int> temp)
+    {
+        //base case
+        if(index==n) {
+            ans.push_back(temp);
+            return;
+        }
+
+        //not included
+        subsets(arr,index+1,n,ans,temp);
+
+        //included
+        temp.push_back(arr[index]);
+        subsets(arr,index+1,n,ans,temp);
     }
-
-    //not included
-    subsets(arr,index+1,n,ans,temp);
-
-    //included
-    temp.push_back(arr[index]);
-    subsets(arr,index+1,n,ans,temp);
-}
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        subsets(nums,0,nums.size(),ans,temp);
+        return ans;
+    }
+};
