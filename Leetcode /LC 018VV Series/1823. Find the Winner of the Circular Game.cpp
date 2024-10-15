b953a504cc05e1e8a7c02f17ae89a02ff40771a3
Vansh Vadhwa <vvad0647@gmail.com>
2024-10-15 21:06:37 +0530

    Update 1823. Find the Winner of the Circular Game.cpp

--- Leetcode /LC 018VV Series/1823. Find the Winner of the Circular Game.cpp
+++ Leetcode /LC 018VV Series/1823. Find the Winner of the Circular Game.cpp
@@ -1,32 +1,14 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
+private:
+    int winner(int n, int k) {
+        //base case
+        if(n==1) {
+            return 0;
+        }
+        return (winner(n-1,k)+k)%n;
+    }
 public:
     int findTheWinner(int n, int k) {
-        vector<int> arr;
-
-        for(int i = 0; i < n; i++) {
-            arr.push_back(i + 1);
-        }
-
-        int index = 0;
-
-        while(arr.size() > 1) { 
-            index = (index + k - 1) % arr.size();
-            arr.erase(arr.begin() + index);
-        }
-
-        return arr[0];
+        return winner(n,k)+1;
     }
 };
-
-int main() {
-    Solution sol;
-    int n = 5;
-    int k = 3;
-    cout << "The winner is: " << sol.findTheWinner(n, k) << endl;
-    return 0;
-}
