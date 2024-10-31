27a66c89cb4aae572ca06fb1d40e3efcde14d64d
Vansh Vadhwa <vvad0647@gmail.com>
2024-10-31 23:39:35 +0530

    Update 258. Add Digits.cpp

--- Leetcode /LC 002VV Series/258. Add Digits.cpp
+++ Leetcode /LC 002VV Series/258. Add Digits.cpp
@@ -1,71 +1,15 @@
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
     int addDigits(int num) {
-        if(num < 10) {
-            return num;
+        int ans = 0;
+        int temp = 0;
+
+        while(num != 0) {
+            ans += num%10;
+            num /= 10;
+            ans = ans%10 + ans/10;
         }
 
-        if(num % 9 == 0) {
-            return 9;
-        }
-
-        if(num % 9 != 0) {
-            return num % 9;git
-            
-        }
-
-        return -1;
-        
-        // int rem = 0;
-        // int ans = 0;
-        
-        // while(num != 0) {
-        //     rem = num % 10;
-        //     num /= 10;
-        //     ans += rem;
-        // }
-
-        // if(ans < 10) {
-        //     return ans;
-        // }
-
-        // int new_ans;
-
-        // if(ans > 10) {
-        //     int new_rem = 0;
-        //     new_ans = 0;
-            
-        //     while(ans != 0) {
-        //         new_rem = ans % 10;
-        //         ans /= 10;
-        //         new_ans += new_rem;
-        //     }
-            
-        //     if(new_ans < 10) {
-        //         return new_ans;
-        //     }
-        // }
-
-        // int final_ans = 0;
-        // int final_rem = 0;
-
-        // while(new_ans != 0) {
-        //     final_rem = new_ans % 10;
-        //     new_ans /= 10;
-        //     final_ans += final_rem;
-        // }
-        
-        // return final_ans;
+        return ans;
     }
 };
-
-int main() {
-    Solution obj;
-    int num = 12345;
-    cout << "Result: " << obj.addDigits(num) << endl;
-    
-    return 0;
-}
