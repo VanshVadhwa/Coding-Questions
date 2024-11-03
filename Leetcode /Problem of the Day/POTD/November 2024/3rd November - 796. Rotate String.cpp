55ae12823c1dc18fa4ec930e930615eeec5d1731
Vansh Vadhwa <vvad0647@gmail.com>
2024-11-03 13:28:56 +0530

    Create 3rd November - 796. Rotate String.cpp

---
+++ Leetcode /Problem of the Day/POTD/November 2024/3rd November - 796. Rotate String.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()) return false;
+        return ((s+s).find(goal)!=string::npos);
+    }
+};
