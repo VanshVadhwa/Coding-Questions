e88babd5497c6fa6cb9c29a1dda8fc7eca5c52ee
Vansh Vadhwa <vvad0647@gmail.com>
2024-11-07 11:19:14 +0530

    Create Smaller on Left.cpp

---
+++ GeeksforGeeks /Stack/Smaller on Left.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int arr[]){
+        // code here
+        vector<int> ans(n,-1);
+        stack<int> st;
+        
+        for(int i=n-1;i>=0;i--) {
+            while(!st.empty() && arr[i]<arr[st.top()]) {
+                ans[st.top()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
