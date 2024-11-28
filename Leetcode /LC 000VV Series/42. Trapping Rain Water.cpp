85e21473a9a30bdc6b4e96b174bfc2ac5c9254ec
Vansh Vadhwa <vvad0647@gmail.com>
2024-11-28 23:32:14 +0530

    Update 42. Trapping Rain Water.cpp

--- Leetcode /LC 000VV Series/42. Trapping Rain Water.cpp
+++ Leetcode /LC 000VV Series/42. Trapping Rain Water.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    int trap(vector<int>& nums) {
-      
-      int n = nums.size();
-      vector<int> maxLeft(n);
-      vector<int> maxRight(n);
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n-1;
+        int leftMax = 0, rightMax = 0;
+        int ans = 0;
 
-        maxLeft[0] = 0;
-        maxRight[n-1] = 0;
-
-        int leftmax = 0;
-        int rightmax = 0;
-
-        for(int i=1;i<n;i++)
-        {
-            leftmax = max(nums[i-1],leftmax);
-            maxLeft[i] = leftmax;
-        }
-
-        for(int i=n-2;i>=0;i--)
-        {
-            rightmax = max(nums[i+1],rightmax);
-            maxRight[i] = rightmax;
-        }
-
-        int minimum = 0;
-        int fill = 0;
-        for(int i=0;i<n;i++)
-        {
-            minimum = min(maxLeft[i],maxRight[i]);
-            if(minimum - nums[i] > 0) {
-                fill += minimum-nums[i];
+        while(left<=right) {
+            if(height[left] <= height[right]) {
+                if(height[left]>=leftMax) {
+                    leftMax = height[left];
+                }
+                else {
+                    ans += leftMax-height[left];
+                }
+                left++;
             }
-        }
-        
-        return fill;
+            else {
+                if(height[right]>=rightMax) {
+                    rightMax = height[right];
+                }
+                else {
+                    ans += rightMax-height[right];
+                }
+                right--;
+            }
+        } 
+
+        return ans;
     }
 };
