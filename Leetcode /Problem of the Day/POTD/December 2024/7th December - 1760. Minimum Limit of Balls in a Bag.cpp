f1025dbfbb59077e120e88c16bb1e0e701d39eda
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-08 22:51:04 +0530

    Create 7th December - 1760. Minimum Limit of Balls in a Bag.cpp

---
+++ Leetcode /Problem of the Day/POTD/December 2024/7th December - 1760. Minimum Limit of Balls in a Bag.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int n = nums.size();
+        int start = 1;
+        int end = *max_element(nums.begin(),nums.end());
+        int ans = end;
+
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+
+            int count = 0;
+
+            for(int i=0;i<n;i++) {
+                count += (nums[i]-1)/mid;
+            }
+
+            if(count <= maxOperations) {
+                ans = mid;
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+
+        return ans;
+        
+    }
+};
