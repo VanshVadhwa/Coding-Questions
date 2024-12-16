e9d432e495582bfb3485f158f3a9383ce1361956
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-17 00:05:39 +0530

    Update 3264. Final Array State After K Multiplication Operations I.cpp

--- Leetcode /LC 032VV Series/3264. Final Array State After K Multiplication Operations I.cpp
+++ Leetcode /LC 032VV Series/3264. Final Array State After K Multiplication Operations I.cpp
@@ -3,18 +3,24 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int n = nums.size();
 
-        while(k) {
-            int minimum = INT_MAX;
-            int index = 0;
-            for(int i=0;i<n;i++) {
-                if(nums[i] < minimum) {
-                    index = i;
-                    minimum = nums[i];
-                }
-            }
-            nums[index] *= multiplier;
-            k--;
+        vector<pair<int,int>> minHeap;
+        for(int i=0;i<n;i++) {
+            minHeap.push_back({nums[i],i});
         }
+
+        // in build function - never knew about it :)
+        make_heap(minHeap.begin(),minHeap.end(),greater<>());
+
+        while(k--) {
+            pop_heap(minHeap.begin(),minHeap.end(),greater<>());
+            auto [value, i] = minHeap.back();
+            minHeap.pop_back();
+            
+            nums[i] *= multiplier;
+            minHeap.push_back({nums[i],i});
+            push_heap(minHeap.begin(),minHeap.end(),greater<>());
+        }
+
         return nums;
     }
 };
