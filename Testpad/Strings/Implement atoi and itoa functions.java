360cc9c935101287f0c0ca79dc5cc6930cf8e234
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-24 23:39:06 +0530

    Update Implement atoi and itoa functions.java

--- Testpad/Strings/Implement atoi and itoa functions.java
+++ Testpad/Strings/Implement atoi and itoa functions.java
@@ -1,24 +1,21 @@
 class Result {
   static String itoa(int num) {
     // Write your code here
+    if(num==0) return "0";
     StringBuilder ans = new StringBuilder();
-    if(num==0) ans.append(0);
-    while(num!=0){
-      ans.append(num%10);
-      num /= 10;
+    while(num != 0) {
+        ans.append(num%10);
+        num /= 10;
     }
     return ans.reverse().toString();
   }
 
   static int atoi(String str) {
     // Write your code here
-    int ans = 0;
-    int index = 0;
-    while(index!=str.length()) {
-      int digit = str.charAt(index)-'0';
-      ans = ans*10 + digit;
-      index++;
-    }
-    return ans;
+      int ans = 0;
+      for(char c : str.toCharArray()) {
+          ans = ans*10 + (c-'0');
+      }
+      return ans;
   }
 }
