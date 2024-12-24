7cba74a45cd30922b632411df10bc54aaa8574b9
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-24 22:36:59 +0530

    Update Rotation count of a sorted Array.java

--- Testpad/Binary Search/Rotation count of a sorted Array.java
+++ Testpad/Binary Search/Rotation count of a sorted Array.java
@@ -1,46 +1,39 @@
 import java.util.Scanner;
 // Other imports go here
 // Do NOT change the class name
-class Main{
-  	static int count(int[] arr, int n)
-    {
-      int start = 0;
-      int end = n-1;
-      
-      if(arr[end]>=arr[start]) {
-        return -1;
-      }
-      
-      while(start <= end)
-      {
-        int mid = start + (end-start)/2;
-        
-        if(mid>0 && arr[mid]<arr[mid-1]) {
-          return mid;
+class Main {
+    private static int rotationCount(int arr[], int n) {
+        // peak index find
+        if(n>1 && arr[0]>arr[1]) return 1;
+        int ans = arr[0];
+        int start = 0;
+        int end = n-1;
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+            if(arr[mid]>=arr[0]) {
+                ans = mid+1;
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
         }
-        else if(arr[start]<=arr[mid]) {
-          start = mid+1;
-        }
-        else {
-          end = mid-1;
-        }
-      }
-      return -1;
+        return ans;
     }
     public static void main(String[] args)
     {
         // Write your code here
-      	Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         while(t-- > 0)
         {
-          int n = input.nextInt();
-          int[] arr = new int[n];
-          for(int i=0;i<n;i++) {
-            arr[i] = input.nextInt();
-          }
-          int ans = count(arr,n);
-          System.out.println(ans);
+            int n = input.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++) {
+                arr[i] = input.nextInt();
+            }
+            if(arr[n-1]>=arr[0]) System.out.println(-1);
+            else System.out.println(rotationCount(arr,n));
         }
         input.close();
     }
