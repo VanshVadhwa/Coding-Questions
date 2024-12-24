03246a55735c768d218abc7fbb20ab010facfd23
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-24 21:47:47 +0530

    Update Binary Equivalent.java

--- Testpad/Recursion/Binary Equivalent.java
+++ Testpad/Recursion/Binary Equivalent.java
@@ -1,13 +1,6 @@
 class Result{ 
-  	static int binary(int n)
-    {
-      //base case
-      if(n<2) {
-        return n;
-      }
-      return binary(n/2)*10 + n%2;
-    }
-    static int decimalToBinary(int n){ 
-      return binary(n);
+    static int decimalToBinary(int n){
+        if(n<2) return n;
+        return decimalToBinary(n/2)*10 + n%2;
     } 
 }
