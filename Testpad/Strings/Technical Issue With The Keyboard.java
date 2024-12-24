b47afe4b55f2456c63bf3eca438764eaa5a75a7b
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-24 23:23:36 +0530

    Update Technical Issue With The Keyboard.java

--- Testpad/Strings/Technical Issue With The Keyboard.java
+++ Testpad/Strings/Technical Issue With The Keyboard.java
@@ -2,17 +2,32 @@ class Result {
   // Return the updated string
   static String getDesiredString(String str) {
     // Write your code here
-    HashMap<Character, Integer> map = new HashMap<>();
-    for(char c : str.toCharArray()) {
-      map.put(c, map.getOrDefault(c,0)+1);
-    }
-    StringBuilder ans = new StringBuilder();
-    for(char c : str.toCharArray()) {
-      if(map.get(c)>0) {
-        ans.append(c);
+      String ans = str.substring(0,1);
+      for(int i=1;i<str.length();i++) {
+          if(str.charAt(i) != str.charAt(i-1)) {
+              ans += str.charAt(i);
+          }
       }
-      map.put(c,0);
-    }
-    return ans.toString();
+      
+      return ans;
   }
 }
+
+// class Result {
+//   // Return the updated string
+//   static String getDesiredString(String str) {
+//     // Write your code here
+//     HashMap<Character, Integer> map = new HashMap<>();
+//     for(char c : str.toCharArray()) {
+//       map.put(c, map.getOrDefault(c,0)+1);
+//     }
+//     StringBuilder ans = new StringBuilder();
+//     for(char c : str.toCharArray()) {
+//       if(map.get(c)>0) {
+//         ans.append(c);
+//       }
+//       map.put(c,0);
+//     }
+//     return ans.toString();
+//   }
+// }
