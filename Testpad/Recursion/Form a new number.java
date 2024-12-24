df66cd4acf07b2b55e27cf85359423ff98396c88
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-24 21:44:20 +0530

    Update Form a new number.java

--- Testpad/Recursion/Form a new number.java
+++ Testpad/Recursion/Form a new number.java
@@ -1,33 +1,26 @@
 import java.util.Scanner;
 // Other imports go here
 // Do NOT change the class name
-class Main{
-  	static int evenNum(int n)
-    {
-      //base case
-      if(n==0) {
-        return 0;
-      }
-      
-      int ans = evenNum(n/10);
-      
-      if((n%10)%2==0) {
-        ans = ans*10 + (n%10);
-      }
- 
-      return ans;
-     }
+class Main {
+    private static int formNumber(int n, int base) {
+        if(n==0) return 0;
+        
+        if(n%2==0) {
+            return n%10*base + formNumber(n/10,base*10);
+        }
+        return formNumber(n/10,base);
+    }
     public static void main(String[] args)
     {
         // Write your code here
-      	Scanner input = new Scanner(System.in);
-      	int t = input.nextInt();
-      	while(t-- > 0)
-        {
-          int n = input.nextInt();
-          int ans = evenNum(n);
-      	  System.out.println(ans);
-        }  
-      	input.close();
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        
+        while(t-- > 0) {
+            int n = input.nextInt();
+            System.out.println(formNumber(n,1));
+        }
+        
+        input.close();
     }
 }
