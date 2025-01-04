58269e3beb8a3607be902d088ef0c69212a4b8bc
Vansh Vadhwa <vvad0647@gmail.com>
2025-01-05 00:28:21 +0530

    Update 560. Subarray Sum Equals K.cpp

--- Leetcode /LC 005VV Series/560. Subarray Sum Equals K.cpp
+++ Leetcode /LC 005VV Series/560. Subarray Sum Equals K.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        int count = 0;
+        long long sum = 0;
+        int ans = 0;
+        unordered_map<int,int> map;
 
-        for(int i=0;i<n;i++) 
-        {
-            int sum = 0;
-            for(int j=i;j<n;j++) {
-                sum += nums[j];
-                if(sum==k) {
-                    count++;
-                }
+        map[0] = 1;
+
+        for(int i : nums) {
+            sum += i;
+
+            if(map.count(sum-k)) {
+                ans += map[sum-k];
             }
+
+            map[sum]++;
         }
-        
-        return count;
+
+        return ans;
     }
 };
