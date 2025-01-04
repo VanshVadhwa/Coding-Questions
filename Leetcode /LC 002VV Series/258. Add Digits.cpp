30eadc3d11101a11dfe6af65d9ca97cde69c4faa
Vansh Vadhwa <vvad0647@gmail.com>
2025-01-04 16:01:57 +0530

    Update 258. Add Digits.cpp

--- Leetcode /LC 002VV Series/258. Add Digits.cpp
+++ Leetcode /LC 002VV Series/258. Add Digits.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int ans = 0;
-        int temp = 0;
-
-        while(num != 0) {
-            ans += num%10;
-            num /= 10;
-            ans = ans%10 + ans/10;
-        }
-
-        return ans;
+        return (num%9==0 && num != 0) ? 9 : num % 9;
     }
 };
