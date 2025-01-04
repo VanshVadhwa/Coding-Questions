3c8a75d7e58bcea6c369fc3cd8ea58c051f0b982
Vansh Vadhwa <vvad0647@gmail.com>
2025-01-05 02:49:31 +0530

    Update 75. Sort Colors.cpp

--- Leetcode /LC 000VV Series/75. Sort Colors.cpp
+++ Leetcode /LC 000VV Series/75. Sort Colors.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                if(nums[j] > nums[j + 1]) {
-                    int temp = nums[j + 1];
-                    nums[j + 1] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-        }
+        // int n = nums.size();
+
+        // selection sort
+        // for(int i=0;i<n-1;i++) {
+        //     int index = i;
+        //     for(int j=i+1;j<n;j++) {
+        //         if(nums[j]<nums[index]) {
+        //             index = j;
+        //         }
+        //     }
+        //     swap(nums[i],nums[index]);
+        // }
+
+        // bubble sort
+        // bool flag = true;
+        // for(int i=0;i<n;i++) {
+        //     for(int j=0;j<n-i-1;j++) {
+        //         if(nums[j]>nums[j+1]) {
+        //             flag = false;
+        //             swap(nums[j],nums[j+1]);
+        //         }
+        //     }
+        //     if(flag) break;
+        // }
+
+        // insertion sort
+        // for(int i=1;i<n;i++) {
+        //     for(int j=i;j>0;j--) {
+        //         if(nums[j]<nums[j-1]) {
+        //             swap(nums[j],nums[j-1]);
+        //         }
+        //         else {
+        //             break;
+        //         }
+        //     }
+        // }
     }
 };
-
-int main() {
-    Solution solution;
-    vector<int> nums = {2, 0, 2, 1, 1, 0};
-
-    cout << "Before sorting: ";
-    for(int num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    solution.sortColors(nums);
-
-    cout << "After sorting: ";
-    for(int num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
