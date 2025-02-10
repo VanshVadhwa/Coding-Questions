0d1593123ff206dfe3017f0f29777688541c6c7b
Vansh Vadhwa <vvad0647@gmail.com>
2025-02-10 12:36:03 +0530

    Create Container With Most Water.cpp

---
+++ GeeksforGeeks /Greedy Algorithms/Container With Most Water.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int left = 0, right = n-1;
+        int ans = 0;
+        
+        while(left < right) {
+            if(arr[left] < arr[right]) {
+                ans = max(ans,arr[left]*(right-left));
+                left++;
+            }
+            else if(arr[left] > arr[right]) {
+                ans = max(ans,arr[right]*(right-left));
+                right--;
+            }
+            else {
+                ans = max(ans,arr[left]*(right-left));
+                left++;
+                right--;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
