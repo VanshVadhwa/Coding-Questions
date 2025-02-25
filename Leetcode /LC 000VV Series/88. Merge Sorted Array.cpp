3a8f45719abacd96d6287aa5dbf69f35322256b6
Vansh Vadhwa <vvad0647@gmail.com>
2025-02-25 22:33:46 +0530

    Update 88. Merge Sorted Array.cpp

--- Leetcode /LC 000VV Series/88. Merge Sorted Array.cpp
+++ Leetcode /LC 000VV Series/88. Merge Sorted Array.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index1 = m-1;
-        int index2 = n-1;
-        int i = m+n-1;
-        
+        int index1 = m-1, index2 = n-1, index = m+n-1;
+
         while(index1 >= 0 && index2 >= 0) {
-            if(nums2[index2]>nums1[index1]) {
-                nums1[i--] = nums2[index2--];
+            if(nums1[index1] >= nums2[index2]) {
+                nums1[index--] = nums1[index1--]; 
             }
             else {
-                nums1[i--] = nums1[index1--];
+                nums1[index--] = nums2[index2--];
             }
         }
-        while(index2>=0) {
-            nums1[i--] = nums2[index2--];
+
+        while(index2 >= 0) {
+            nums1[index--] = nums2[index2--];
         }
     }
 };
