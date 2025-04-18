7bb5af05180c827dcdc8c2f9a7893dc06723a346
Vansh Vadhwa <vvad0647@gmail.com>
2025-04-18 20:28:34 +0530

    Update 179. Largest Number.cpp

--- Leetcode /LC 001VV Series/179. Largest Number.cpp
+++ Leetcode /LC 001VV Series/179. Largest Number.cpp
@@ -1,26 +1,27 @@
+bool comp(string a, string b) {
+    return a+b > b+a;
+}
+
 class Solution {
-private:
-    static bool compare(string& s1, string& s2) {
-        return s1+s2 > s2+s1;
-    }
 public:
     string largestNumber(vector<int>& nums) {
         int n = nums.size();
-        vector<string> ans(n,"");
+        vector<string> numsString(n,"");
 
         for(int i=0;i<n;i++) {
-            ans[i] = to_string(nums[i]);
+            numsString[i] = to_string(nums[i]);
         }
 
-        sort(ans.begin(),ans.end(), compare); //custom comparator
+        sort(numsString.begin(),numsString.end(), comp);
 
-        string output = "";
-        for(string str : ans) {
-            output += str;
+        string ans = "";
+
+        for(string s : numsString) {
+            ans += s;
         }
 
-        if(output[0]=='0') return "0";
+        if(ans[0]=='0') return "0";
 
-        return output;
+        return ans;
     }
 };
