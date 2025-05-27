405d5b584852fcbb42b07389e042e24753c324e2
Vansh Vadhwa <vvad0647@gmail.com>
2025-05-27 14:44:45 +0530

    Create 27th May - Divisible and Non-divisible Sums Difference.cpp

---
+++ Leetcode /Problem of the Day/POTD/May 2025/27th May - Divisible and Non-divisible Sums Difference.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int ans = 0;
+        for(int i=1;i<=n;i++) {
+            ans = (i%m!=0) ? ans + i : ans - i;
+        }
+        return ans;
+    }
+};
