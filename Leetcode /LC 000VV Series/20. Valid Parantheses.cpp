6a58eb79f5a51d58b7278690eb296f2491e1f0bb
Vansh Vadhwa <vvad0647@gmail.com>
2025-05-29 23:38:49 +0530

    Update 20. Valid Parantheses.cpp

--- Leetcode /LC 000VV Series/20. Valid Parantheses.cpp
+++ Leetcode /LC 000VV Series/20. Valid Parantheses.cpp
@@ -4,18 +4,16 @@ public:
         stack<char> st;
 
         for(char c : s) {
-            if(c=='(' || c=='[' || c=='{') {
-                st.push(c);
-            }
+            if(c=='(' || c=='{' || c=='[') st.push(c);
             else {
                 if(st.empty()) return false;
-                if(c==')' && st.top()!='(') return false;
-                if(c=='}' && st.top()!='{') return false;
-                if(c==']' && st.top()!='[') return false;
+                if(c==')' && st.top() != '(') return false;
+                if(c=='}' && st.top() != '{') return false;
+                if(c==']' && st.top() != '[') return false;
                 st.pop();
             }
         }
 
-        return st.empty();
+        return !st.size();
     }
 };
