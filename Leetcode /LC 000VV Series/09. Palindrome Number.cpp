6396ae9f7f3d0d526a37fd8c23f2f2bc6f29b9a2
Vansh Vadhwa <vvad0647@gmail.com>
2025-06-27 06:37:03 +0530

    Update 09. Palindrome Number.cpp

--- Leetcode /LC 000VV Series/09. Palindrome Number.cpp
+++ Leetcode /LC 000VV Series/09. Palindrome Number.cpp
@@ -1,61 +1,14 @@
-#include <iostream>
-using namespace std;
-
-// class Solution {
-//     int reverse(int num) {
-//         if (num < 0) {
-//             return false;
-//         }
-//         long long rev = 0;
-
-//         while (num != 0) {
-//             int digit = num % 10;
-//             rev = rev * 10 + digit;
-//             num = num / 10;
-//         }
-//         return rev;
-//     }
-// public:
-//     bool isPalindrome(int x) {
-//         int num = reverse(x);
-//         return num == x;
-//     }
-// };
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) {
-            return false;
-        }
-
-        int ans = 0;
-        int rem;
-        int original = x;
-
-        while(x!=0) {
-            rem = x%10;
+        if(x < 0) return false;
+        int temp = x, rev = 0;
+        while(x > 0) {
+            int rem = x%10;
+            if(rev > INT_MAX/10 || rev < INT_MIN/10) return false;
+            rev = rev*10 + rem;
             x /= 10;
-            if(ans > INT_MAX/10) {
-                return 0;
-            }
-            ans = ans*10 + rem;
         }
-
-        if(ans == original) {
-            return true;
-        }
-
-        return false;
+        return temp==rev; 
     }
 };
-
-int main() {
-    Solution solution;
-    int x = 121; 
-    if (solution.isPalindrome(x)) {
-        cout << x << " is a palindrome." << endl;
-    } else {
-        cout << x << " is not a palindrome." << endl;
-    }
-    return 0;
-}
