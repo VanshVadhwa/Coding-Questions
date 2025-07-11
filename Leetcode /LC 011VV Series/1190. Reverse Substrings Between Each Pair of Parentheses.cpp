9289671b806b3b31539ad1871e56d977f7b4be1b
Vansh Vadhwa <vvad0647@gmail.com>
2025-07-11 10:16:21 +0530

    Update 1190. Reverse Substrings Between Each Pair of Parentheses.cpp

--- Leetcode /LC 011VV Series/1190. Reverse Substrings Between Each Pair of Parentheses.cpp
+++ Leetcode /LC 011VV Series/1190. Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -1,51 +1,30 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
+private:
+    void reverse(string& s, int start, int end) {
+        while(start < end) {
+            swap(s[start++],s[end--]);
+        }
+    }
 public:
     string reverseParentheses(string s) {
         int n = s.length();
-        vector<int> ans;
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
-                ans.push_back(i);
-            } 
-            else if (s[i] == ')') {
-                reverse(s.begin() + ans.back(), s.begin() + i);
-                ans.pop_back();
+        stack<int> st;
+
+        for(int i=0;i<n;i++) {
+            if(s[i]=='(') {
+                st.push(i);
+            }
+            else if(s[i]==')') {
+                reverse(s,st.top()+1,i-1);
+                st.pop();
             }
         }
 
-        string answer;
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] != '(' && s[i] != ')') {
-                answer.push_back(s[i]);
-            }
-        }
-
-        return answer;
+        
+        string ans = "";
+        for(char c : s) if(c!='(' && c!=')') ans += c;
+        
+        return ans;
     }
 };
-
-int main() {
-    Solution sol;
-
-    string s1 = "(ed(et(oc))el)";
-    string result1 = sol.reverseParentheses(s1);
-    cout << "Input: " << s1 << "\nOutput: " << result1 << endl; // Expected: "leetcode"
-
-    string s2 = "(u(love)i)";
-    string result2 = sol.reverseParentheses(s2);
-    cout << "Input: " << s2 << "\nOutput: " << result2 << endl; // Expected: "iloveu"
-
-    string s3 = "(ab(cd(ef)gh)ij)";
-    string result3 = sol.reverseParentheses(s3);
-    cout << "Input: " << s3 << "\nOutput: " << result3 << endl; // Expected: "jihgfeabc"
-
-    return 0;
-}
