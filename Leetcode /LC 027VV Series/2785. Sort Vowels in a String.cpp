4b49f95b3dc670f2f6962d5126b0fd9bc4e45829
Vansh Vadhwa <vvad0647@gmail.com>
2025-09-11 10:12:28 +0530

    Update 2785. Sort Vowels in a String.cpp

--- Leetcode /LC 027VV Series/2785. Sort Vowels in a String.cpp
+++ Leetcode /LC 027VV Series/2785. Sort Vowels in a String.cpp
@@ -1,58 +1,15 @@
 class Solution {
+private:
+    bool isVowel(char c) {
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' ||c=='I' || c=='O' || c=='U';
+    }   
 public:
     string sortVowels(string s) {
-        int n = s.length();
-        vector<int> lower(26, 0);
-        vector<int> upper(26, 0);
-
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') 
-            {
-                lower[s[i]-'a']++;
-                s[i] = '#';
-            }
-            else if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')      {
-                upper[s[i]-'A']++;
-                s[i] = '#';
-            }
+        priority_queue<char,vector<char>,greater<char>> pq;
+        for(char c : s) if(isVowel(c)) pq.push(c);
+        for(char& c : s) if(isVowel(c)) {
+            c = pq.top();pq.pop();
         }
-
-        //Answer string
-        string ans = "";
-
-        //uppercase
-        for(int i=0;i<26;i++)
-        {
-            char c = 'A'+i;
-            while(upper[i]) {
-                ans += c;
-                upper[i]--;
-            }
-        }
-
-        //lowercase
-        for(int i=0;i<26;i++)
-        {
-            char c = 'a'+i;
-            while(lower[i]) {
-                ans += c;
-                lower[i]--;
-            }
-        }
-
-        // final answer
-        int first = 0, second = 0;
-        while(second < ans.size())
-        {
-            if(s[first]=='#') 
-            {
-                s[first] = ans[second];
-                second++;
-            }
-            first++;
-        }
-
         return s;
     }
 };
